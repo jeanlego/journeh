3ec952afd202e6b1e8f471c88f1ab9130ae96372
jeanlego (Jean-Philippe Legault) <jeanphilippe.legault@unb.ca>
2020-09-22 19:22:29 -0300

    fix new entries to use word diff

--- journ.sh
+++ journ.sh
@@ -4,21 +4,30 @@ JOURNAL_DIR="$HOME/.journeh"
 DATE="$(date -u +%F)"
 
 function new_entry() {
-	touch $1
-	vim "+normal G$" +startinsert $1
-	git add .
+	printf "%s\n\n" "${*:2}" > $JOURNAL_DIR/.$1.md
+        awk "/$2/,EOF" $JOURNAL_DIR/$1.md | tail -n +2 >> $JOURNAL_DIR/.$1.md
+	
+	vim "+normal G$" +startinsert $JOURNAL_DIR/.$1.md
+	if ! wdiff -1 -2 -3 -n \
+  	 -w $'\033[30;31m' -x $'\033[0m' \
+  	 -y $'\033[30;32m' -z $'\033[0m' \
+	 $JOURNAL_DIR/$1.md $JOURNAL_DIR/.$1.md &> /dev/null;
+	then
+		mv -f $JOURNAL_DIR/.$1.md $JOURNAL_DIR/$1.md
+		git add .
+		git commit -m "update $2"
+		git push
+	fi
 }
 
 function daily() {
-	new_entry "$JOURNAL_DIR/$DATE.md"
-	
-        git commit -m "update daily entry $DATE"
+	new_entry "$DATE" "# Entry $DATE"
 }
 
 function weekly() {
 	FIRST_DAY=$(date -u -d "sunday - $(( $(date -u +%s) - $(date -u -d "$DATE" +%s) ) / 86400 )) days" +%F)
 	WEEK_NAME=$(date -u -d "$file_date" +%Y-%V)
-	printf "# %s\n" $WEEK_NAME > $JOURNAL_DIR/.$WEEK_NAME.md
+	printf "# %s\n" "$WEEK_NAME" > $JOURNAL_DIR/.$WEEK_NAME.md
 	
 	for $(( i=0; i<7; i += 1 ));
 	do
@@ -26,22 +35,21 @@ function weekly() {
 		printf "\n## %s\n" $(date -u -d "$file_date" +%A) >> $JOURNAL_DIR/.$WEEK_NAME.md
 		if [ -f $file_date ];
 		then
-			cat $JOURNAL_DIR/$file_date.md >> $JOURNAL_DIR/.$WEEK_NAME.md
+			# drop the header
+		        awk "/# /,EOF" $JOURNAL_DIR/$file_date.md | tail -n +2 >> $JOURNAL_DIR/.$WEEK_NAME.md
 		fi
 	done
-
-	echo "## Weekly Summary" > $JOURNAL_DIR/.$WEEK_NAME.md 
-	awk '/## Weekly Summary/,EOF' | tail -n +2 >> $JOURNAL_DIR/.$WEEK_NAME.md
-	mv -f $JOURNAL_DIR/.$WEEK_NAME.md $JOURNAL_DIR/$WEEK_NAME.md
 	
-	new_entry "$JOURNAL_DIR/$WEEK_NAME.md"
-
-        git commit -m "update weekly entry for week $WEEK_NAME starting on $FIRST_DAY"
+	new_entry "$WEEK_NAME" "## Weekly Summary"
 }
 
 if [ "_$1" != "_init" ] && [ ! -d ${JOURNAL_DIR} ];
 then
 	echo "Your journal directory has not been initialized, please run 'init <repo url>' first"
+else
+	cd ${JOURNAL_DIR}
+	# clean old temp files
+	rm ./.*.md &> /dev/null
 fi
 
 case "_$1" in
@@ -51,12 +59,10 @@ case "_$1" in
 	_day)
 		[ "_${*:2}" != "_" ] && DATE=$(date -u -d "${*:2}" +%F)
 		daily $DATE
-		git push
 		;;
 	_week)
 		[ "_${*:2}" != "_" ] && DATE=$(date -u -d "${*:2}" +%F)
 		weekly $DATE
-		git push
 		;;
 	_help|_)
 		echo "
