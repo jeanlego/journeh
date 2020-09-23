ab623ebd6322dedb0fc80e2de63ee3b3c186dbaa
jeanlego (Jean-Philippe Legault) <jeanphilippe.legault@unb.ca>
2020-09-23 00:07:24 -0300

    add yearly and quarterly report

--- journ.sh
+++ journ.sh
@@ -1,43 +1,58 @@
 #!/bin/bash
 
+THIS_DIR=$(dirname $(readlink -f $0))
 JOURNAL_DIR="$HOME/.journeh"
 DATE="$(date +%F)"
 
 function init_file() {
-    if [ ! -f "$JOURNAL_DIR/$1.md" ];
+    if [ ! -f "$1" ];
     then
-        printf "# %s\n\n" "$1" >> "$JOURNAL_DIR/$1.md"
+        template="$JOURNAL_DIR/.template.md"
+        [ ! -f template ] && template="${PWD}/template.md"
+        [ ! -f template ] && template="${THIS_DIR}/template.md"
+
+        for VARNAME in $(grep -P -o -e '\$[\{]?(\w+)*[\}]?' "$template" | sort -u); do     
+            VARNAME2=$(echo "$VARNAME"| sed -e 's|^\${||g' -e 's|}$||g' -e 's|^\$||g' );
+            VAR_VALUE2=${!VARNAME2};
+
+            if [ "_" != "_$VAR_VALUE2" ]; then
+                sed  "s|$VARNAME|$VAR_VALUE2|g" "$template" >> "$1"; 
+            fi      
+        done
     fi
 }
 
 function daily() {
-    init_file "$DATE"
+    PERIOD="$DATE"
+    init_file "$JOURNAL_DIR/$DATE.md"
+
     vim "+normal G$" "$JOURNAL_DIR/$DATE.md"
 }
 
 function periodic() {
-    FIRST_DAY=$(date -d "$DATE - $(date -d "$DATE - 1 days" +"$2") days" +%F)
-    PERIOD_NAME=$(date -d "$DATE" +"%Y-$1$3")
-    echo "period -- $4"
+    PERIOD=$(date -d "$DATE" +"%Y$2$3")
     # import all the files into one
     for (( i=0; i<=$4; i += 1 ));
     do
-        filename="$(date -d "$FIRST_DAY + $i days" +%F)"
+        filename="$(date -d "$1 + $i days" +%F)"
         if (( i == $4 ));
         then
-            filename=$PERIOD_NAME
+            filename=$PERIOD
+            init_file "$JOURNAL_DIR/$PERIOD.md"
         fi
 
-        init_file "$filename"
-        {
-            [ -n "$(tail -c1 "$JOURNAL_DIR/$filename.md")" ] && printf "\n"
-            printf "<!--BEGIN-%s-->\n" "$filename";
-            cat "$JOURNAL_DIR/$filename.md";
-        } >> "$JOURNAL_DIR/.$PERIOD_NAME.md"
+        if [ -f "$JOURNAL_DIR/$filename.md" ];
+            then
+            {
+                [ -n "$(tail -c1 "$JOURNAL_DIR/$filename.md")" ] && printf "\n"
+                printf "<!--BEGIN-%s-->\n" "$filename";
+                cat "$JOURNAL_DIR/$filename.md";
+            } >> "$JOURNAL_DIR/.$PERIOD.md"    
+        fi
     done
     
     # open for changes
-    vim "+normal G$" "$JOURNAL_DIR/.$PERIOD_NAME.md"
+    vim "+normal G$" "$JOURNAL_DIR/.$PERIOD.md"
 
     filename=""
     while IFS="" read -r lines || [ -n "$lines" ]
@@ -53,7 +68,7 @@ function periodic() {
                 echo "$lines" >> "$filename"
             ;;
         esac
-    done < "$JOURNAL_DIR/.$PERIOD_NAME.md"
+    done < "$JOURNAL_DIR/.$PERIOD.md"
 }
 
 if [ "_$1" != "_init" ] && [ ! -d "$JOURNAL_DIR" ];
@@ -76,11 +91,27 @@ case "_$1" in
         ;;
     _week)
         [ "_${*:2}" != "_" ] && DATE=$(date -d "${*:2}" +%F)
-        periodic "W" "%u" "%W" 7
+        days_in_week=7
+        FIRST_DAY=$(date -d "$DATE - $(date -d "$DATE - 1 days" +%u) days" +%F)
+        periodic "$FIRST_DAY" "-W" "%W" $days_in_week
         ;;
     _month)
+        [ "_${*:2}" != "_" ] && DATE=$(date -d "${*:2}" +%F)
         days_in_month=$(cal "$DATE" | awk 'NF {DAYS = $NF}; END {print DAYS}')
-        periodic "M" "%d" "%m" "$days_in_month"
+        FIRST_DAY=$(date -d "$DATE - $(date -d "$DATE - 1 days" +%d) days" +%F)
+        periodic "$FIRST_DAY" "-M" "%m" "$days_in_month"
+        ;;
+    _quarter)
+        [ "_${*:2}" != "_" ] && DATE=$(date -d "${*:2}" +%F)
+        current_quarter="$(date -d "$DATE" +%q)"
+        days_in_quarter=91 # the last two days are throw aways anyways
+        FIRST_DAY=$(( days_in_quarter * ( current_quarter - 1 ) ))
+        periodic "$FIRST_DAY" "-Q" "%q" "$days_in_quarter"
+        ;;
+    _year)
+        [ "_${*:2}" != "_" ] && DATE=$(date -d "${*:2}" +%F)
+        days_in_year=365 # dont bother with the odd year
+        periodic "1" "" "" "$days_in_year"
         ;;
     _help|_)
         echo "
---
+++ template.md
@@ -0,0 +1,13 @@
+# ${PERIOD}
+
+## Outcome
+
+## Summary
+
+### What was planned
+
+### What I have achieved
+
+### What am I blocked on
+
+### What I want to do next
